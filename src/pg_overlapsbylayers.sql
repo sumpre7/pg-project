91d0dda2f5bed6ce04595870415f40737f82d032
supreme7 <907331074@qq.com>
2021-04-10 14:18:27 +0800

    处理图层可能不存在导致的报错情况

--- src/pg_overlapsbylayers.sql
+++ src/pg_overlapsbylayers.sql
@@ -7,22 +7,23 @@ DECLARE
     number_relationlayer integer := array_length(relationlayers, 1);
     relationlayer_index integer := 1;
     ii integer :=1;
-    num integer :=1;
 BEGIN
     FOR  ii IN 1..number_relationlayer-1 LOOP
-            relationlayer_index = num;
+            relationlayer_index = ii;
             WHILE relationlayer_index <= number_relationlayer LOOP
                     relationlayer_index = relationlayer_index + 1;
+                    if( to_regclass(relationlayers[ii]) is null or  to_regclass(relationlayers[relationlayer_index]) is null ) then
+                        return;
+                    end if;
                     RETURN QUERY EXECUTE
                     'select a.'||checkuniqueid||',b.'||checkuniqueid||' as referenceCheckentityCheckuniqueid ,
 					 a.'||displayname||' as bsm  , b.'||displayname||' as refbsm ,
-				  	'''||relationlayers[num]||''' as layer , '''||relationlayers[relationlayer_index]||''' as reflayer
-						from '||relationlayers[num]||' a inner join '||relationlayers[relationlayer_index]||' b on  st_isvalid(a.'||fieldname||') and st_isvalid(b.'||fieldname||') and
+				  	'''||relationlayers[ii]||''' as layer , '''||relationlayers[relationlayer_index]||''' as reflayer
+						from '||relationlayers[ii]||' a inner join '||relationlayers[relationlayer_index]||' b on  st_isvalid(a.'||fieldname||') and st_isvalid(b.'||fieldname||') and
 						st_intersects(a.'||fieldname||',b.'||fieldname||')
 					and ST_Area(st_intersection(a.'||fieldname||',b.'||fieldname||')) > '||tolerance||'';
                     relationlayer_index = relationlayer_index + 1;
                 END LOOP;
-            num = num +1;
         END LOOP;
 END;
 $$ language 'plpgsql';
